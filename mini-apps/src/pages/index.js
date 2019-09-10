b16f6da50575ea0cf267c3778b85ef07288a70aa
Savvas Sheittanis <sheittanis@hotmail.com>
2019-09-10 21:45:33 +0100

    Menu and dropdown. Some styling

--- mini-apps/src/pages/index.js
+++ mini-apps/src/pages/index.js
@@ -2,19 +2,31 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+const appChanged = () =>{
+  console.log("test")
+}
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    
+    <div>
+      <label htmlFor="appSelector">Select App</label>
+      <select id="appSelector" name="appSelector" onChange={appChanged}>
+        {/* <option value="" selected>Select app</option> */}
+        <option value="Mr">Roll a dice</option>
+        <option value="Miss">Coin Flip</option>
+        <option value="Mrs">8 Ball</option>
+        <option value="Mrs">RNGesus</option>
+      </select>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+
+    <Link to="/page-2/">Go to page 2 (todo remove)</Link>
   </Layout>
 )
 
